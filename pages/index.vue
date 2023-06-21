39dfc2b57592918fab427f6402388ca59eacc6c9
Pascal Koop <pk@megorei.com>
2023-06-21 14:10:20 +0200

    Learn routing with nuxt wird wieder entfernt sobald es richtig losgeht

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+
+
+<template>
+  <div>
+   <Alerts />
+  </div>
+</template>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style
