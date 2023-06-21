b4949a24949a9ba2894a4b2a8d46d63d950abcdd
Pascal Koop <pk@megorei.com>
2023-06-21 09:30:05 +0200

    Add configuration to catch everything and import dynamically

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
+import { resolve} from 'path';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  alias: {
+    // path of current project root folder  
+    '@': resolve(__dirname, '/'),
+  }
 })
