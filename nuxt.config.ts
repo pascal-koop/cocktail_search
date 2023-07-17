63daa47e5f5f4937ff04663b20014943f04bd2c5
Matthias Döring <md@megorei.com>
2023-07-17 02:51:54 +0200

    Add deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,24 @@
-import { resolve} from 'path';
+import { resolve } from 'path';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   alias: {
-    // path of current project root folder  
+    // path of current project root folder
     '@': resolve(__dirname, '/'),
-  }, 
+  },
   css: [
     '~/assets/main.css',
-    ],
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
-    
-    runtimeConfig: {
-      // Will be available in both server and client
-      apiBaseURL: process.env.API_BASE_URL,
-      apiToken: process.env.API_TOKEN,
-    },
-    
+  },
 
+  runtimeConfig: {
+    // Will be available only on server-side
+    apiBaseUrl: '',
+    apiToken: '',
+  }
 })
--- server/api/ingredients.get.ts
+++ server/api/ingredients.get.ts
@@ -1,17 +1,16 @@
 export default defineEventHandler(async event => {
-  const { apiBaseURL, apiToken } = useRuntimeConfig();
-  const url: string = `${apiBaseURL}/items/ingredients?fields=title,id`;
+  const { apiBaseUrl, apiToken } = useRuntimeConfig();
+  const url: string = `${apiBaseUrl}/items/ingredients?fields=title,id`;
   try {
-    const data = await fetch( url , {
+    const data = await fetch(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${apiToken}`,
       },
     });
-    return data.json(); 
-  } catch(error){
+    return data.json();
+  } catch (error) {
     console.log(error)
   }
-  
 });
