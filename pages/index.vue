d71ca92453bdcca93d38625802aaf7106206e66c
Matthias Döring <md@megorei.com>
2023-07-24 08:44:45 +0000

    Add server route to fetch images from directus

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,7 @@
 
 <template>
   <div>
+   <img class="w-1/2" src="/img/65559b5d-b1d9-40a0-a333-15c394314b0d" alt="Test image">
    <h1 class="text-2xl">Ingredients</h1>
   <ul>
     <li v-for="ingredient in ingredientsWithTitle" :key="ingredient">
---
+++ server/routes/img/[uuid].get.ts
@@ -0,0 +1,29 @@
+export default defineEventHandler(async (event) => {
+  const { apiBaseUrl, apiToken } = useRuntimeConfig()
+  const { uuid } = event.context!.params!
+  const url: string = `${apiBaseUrl}/assets/${uuid}`
+
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${apiToken}`,
+      },
+    });
+
+    const contentType = response.headers.get('Content-Type')
+    const contentLength = response.headers.get('Content-Length')
+
+    setResponseHeaders(event, {
+      'Cache-Control': 'private, max-age=31536000',
+      'Content-Type': contentType!,
+      'Content-Length': contentLength!,
+    })
+
+    const arrayBuffer = await response.arrayBuffer()
+
+    return Buffer.from(arrayBuffer);
+  } catch (error) {
+    console.log(error)
+  }
+})
