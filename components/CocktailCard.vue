675a1ba158baedcab5e03f6881d82c8340b18fd0
Pascal Koop <pk@megorei.com>
2023-07-25 15:35:03 +0200

    Format document

--- components/CocktailCard.vue
+++ components/CocktailCard.vue
@@ -1,12 +1,7 @@
 <script setup>
-const props = defineProps([
-  'title',
-  'description',
-  'imgId',
-  'uuid'
-])
+const props = defineProps(['title', 'description', 'imgId', 'uuid']);
 
-const imgUrl = computed(() => `/img/${props.imgId}`)
+const imgUrl = computed(() => `/img/${props.imgId}`);
 </script>
 
 <style></style>
@@ -15,15 +10,17 @@ const imgUrl = computed(() => `/img/${props.imgId}`)
   <div
     class="container font-roboto w-80 rounded-xl drop-shadow-lg bg-fuchsia-50 py-1 my-8 mx-auto border border-stone-300"
   >
-    <h1 class="p-3 text-zinc-900 text-base font-thin leading-normal tracking-tight">
-    {{ title  }}
+    <h1
+      class="p-3 text-zinc-900 text-base font-thin leading-normal tracking-tight"
+    >
+      {{ title }}
     </h1>
     <img class="object-cover" :src="imgUrl" alt="cocktail" />
     <div class="container">
       <p
         class="font-roboto text-sm font-thin leading-tight tracking-tight text-zinc-600 text-left m-3 break-words max-w-xs line-clamp-2"
       >
-        {{ description  }}
+        {{ description }}
       </p>
     </div>
     <div class="flex justify-end flex-row mx-3 mt-6 mb-3">
