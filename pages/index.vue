aebe02948b399f089bf5d9c33c13932fdab7d7ff
Pascal Koop <pk@megorei.com>
2023-08-24 10:47:25 +0200

    Formatierungen

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <script setup>
-const { data } = await $fetch('/api/ingredients');
+const { data } = await $fetch("/api/ingredients");
 </script>
 
 <style></style>
 
 <template>
-  <div class="container flex items-center flex-col">
-    <h1
-      class="text-gray-700 text-4xl text-dark-petrol uppercase mx-auto mt-20"
-    >
+  <div class="container flex items-center flex-col mx-auto">
+    <h1 class="text-gray-700 text-4xl text-dark-petrol uppercase mx-auto mt-20">
       ^Cocktails^
     </h1>
     <h2
